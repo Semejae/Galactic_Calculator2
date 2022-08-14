a57e0f758901c298f8e02024fb2f154ba300c634
Anthony Gladden <a.gladden360@outlook.com>
2022-08-14 10:17:17 -0700

    added earth and mars test

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -4,8 +4,20 @@ import Astro from './../src/js/planet.js'
 describe ('Astro', () => {
 
   test('should return astro object properties', () => {
-    const person = new Astro(50, 'mars');
-    expect(person.age).toEqual(50);
+    const person = new Astro(30, 'mars');
+    expect(person.age).toEqual(30);
+    expect(person.planet).toEqual('mars');
   });
-});  
 
+describe('Astro#currentYears', () => {
+
+  test("should create a prototype for user age based on the value 'earth' for the planet property ",() =>{
+    const person = new Astro(30,'earth');
+    expect(person.planet).toEqual('earth');
+    expect(person.currentYears()).toEqual(30);
+  });
+  
+  test("should create a prototype for user age based on the value 'mars' for the planet property ",() =>{
+    const person = new Astro(30, 'mars');
+    expect(person.planet).toEqual('mars');
+    expect(person.currentYears()).toEqual(15.95);
--- src/js/planet.js
+++ src/js/planet.js
@@ -6,21 +6,6 @@ export default class Astro {
     this.planet = planet;
   }
 
-  convertToYears(){
-    this.mercuryAge = parseInt((this.age/.24).toFixed(0));
-    this.venusAge = parseInt((this.age/.62).toFixed(0));
-    this.marsAge = parseInt((this.age/1.88).toFixed(0));
-    this.jupiterAge = parseInt((this.age/11.86).toFixed(0));
-    this.earthAge = parseInt((this.age/1).toFixed(0));
-    this.yearsLeftToLive = parseInt((this.age));
-  } 
-
-  lifeExpectancy(){
-    if (this.age < this.expectancy) {
-      this.yearsLeftToLive = this.expectancy - this.age;
-    } else (this.age > this.expectancy)
-      this.yearsOverExpectancy = this.age - this.expectancy;
-  };
+  
 
 
-};
